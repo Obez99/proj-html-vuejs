60e406400c3fc8dd5a47a09f52fbbe1bf53f1c67
Obez99 <giuliogx@live.it>
2021-11-22 15:35:57 +0100

    Adds header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header></Header>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
+  components: { Header },
 };
 </script>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap.scss";
+
+a {
+  color: black;
+  text-decoration: none;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <div class="container">
+      <img src="../assets/dark-logo.png" alt="logo" />
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home <i class="fa fa-chevron-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Pages <i class="fa fa-chevron-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Courses <i class="fa fa-chevron-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Features <i class="fa fa-chevron-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Blog <i class="fa fa-chevron-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Shop <i class="fa fa-chevron-down"></i></a>
+          </li>
+        </ul>
+
+        <div>
+          <a href="#"><i class="fa fa-user-o"></i></a>
+          <input type="text" class="form-control" placeholder="Search..." />
+          <i class="fa fa-search"></i>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss">
+@import "../styles/header.scss";
+</style>
