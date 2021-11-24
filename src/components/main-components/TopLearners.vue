1f3e19d75bf24b24ee19e15ba98366d49a919e6d
Obez99 <giuliogx@live.it>
2021-11-24 15:24:40 +0100

    Makes TopLearners component dynamic by adding Review component

--- src/components/main-components/TopLearners.vue
+++ src/components/main-components/TopLearners.vue
@@ -8,60 +8,11 @@
 
     <!--Reviews container!-->
     <div class="reviews-container">
-      <!--Card description!-->
-      <div class="review-card">
-        <h5>It's a choice of quality for people with special needs</h5>
-        <p>
-          I'm a very strict person so I require everything to be organized and
-          neat. Then, I'll be able to make things right and shine. MaxCoach guys
-          just got me.
-        </p>
-
-        <!--Card author!-->
-        <div class="review-author">
-          <img src="../../assets/testimonial-avata-03.jpg" alt="mina-hollace" />
-          <div class="author-info">
-            <h6>FLORENCE THEMES</h6>
-            <small>/ Multimedia Admin</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="review-card active">
-        <h5>High level of efficiency and scientific teaching methods</h5>
-        <p>
-          I am free to learn at my own pace, follow my own schedule and choose
-          the subject I want to learn from the syllabus. Great study portal for
-          people like me.
-        </p>
-
-        <div class="review-author">
-          <img src="../../assets/testimonial-avata-02.jpg" alt="mina-hollace" />
-          <div class="author-info">
-            <h6>MINA HOLLACE</h6>
-            <small>/ Freelancer</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="review-card">
-        <h5>
-          Professional team of specialists and passionate mentors to reach
-        </h5>
-        <p>
-          I need to get a certification for English proficiency and MaxCoach is
-          my best choice. Their tutors are smart and professional when dealing
-          with students.
-        </p>
-
-        <div class="review-author">
-          <img src="../../assets/testimonial-avata-04.jpg" alt="mina-hollace" />
-          <div class="author-info">
-            <h6>MADLEY PONDOR</h6>
-            <small>/ IT Specialist</small>
-          </div>
-        </div>
-      </div>
+      <Review
+        v-for="(review, i) in reviewsList"
+        :key="i"
+        :review="reviewsList[i]"
+      ></Review>
     </div>
 
     <!--Cards navigation dots!-->
@@ -88,8 +39,50 @@
 </template>
 
 <script>
+import Review from "./main-sub-components/Review.vue";
 export default {
   name: "TopLearners",
+  components: { Review },
+  data() {
+    return {
+      reviewsList: [
+        {
+          title: "It's a choice of quality for people with special needs",
+          description:
+            "I'm a very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just go me.",
+          active: false,
+          author: {
+            name: "Florence Themes",
+            photo: require("../../assets/testimonial-avata-03.jpg"),
+            role: "Multimedia Admin",
+          },
+        },
+        {
+          title: "High level of efficiency and scientific teaching methods",
+          description:
+            "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+          active: true,
+          author: {
+            name: "Mina Hollace",
+            photo: require("../../assets/testimonial-avata-02.jpg"),
+            role: "Freelancer",
+          },
+        },
+        {
+          title:
+            "Professional team of specialists and passionate mentors at reach",
+          description:
+            "I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+          active: false,
+          author: {
+            name: "Madley Pondor",
+            photo: require("../../assets/testimonial-avata-04.jpg"),
+            role: "IT Specialist",
+          },
+        },
+      ],
+    };
+  },
 };
 </script>
 
---
+++ src/components/main-components/main-sub-components/Review.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="review-card" :class="review.active ? 'active' : ''">
+    <h5>{{ review.title }}</h5>
+    <p>{{ review.description }}</p>
+
+    <!--Card author!-->
+    <div class="review-author">
+      <img :src="review.author.photo" :alt="review.author.name" />
+      <div class="author-info">
+        <h6 class="text-uppercase">{{ review.author.name }}</h6>
+        <small>/ {{ review.author.role }}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Review",
+  props: { review: Object },
+};
+</script>
+
+<style>
+</style>
